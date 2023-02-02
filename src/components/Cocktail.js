9410dae31bdd04c01fe7620fe2906fd44a58833f
TARAPARADARSHIT <86649636+TARAPARADARSHIT@users.noreply.github.com>
2023-02-02 22:27:21 +0530

    fix

--- src/components/Cocktail.js
+++ src/components/Cocktail.js
@@ -1,4 +1,5 @@
 import { useGlobalContext } from 'context'
+import Error from 'pages/Error';
 import React from 'react'
 import { Link } from 'react-router-dom'
 import CocktailList from './CocktailList';
@@ -6,16 +7,18 @@ import Loading from './Loading';
 
 
 const Cocktail = (props) => {
-  const { coocktailItems } = useGlobalContext();
+  const { coocktailItems, hasError } = useGlobalContext();
 
 
   return (
     <div className='cocktails-center mt-10'>
-      {coocktailItems?.length > 0 ? coocktailItems.map(({ idDrink, strCategory, strAlcoholic, strDrinkThumb, strDrink }) => {
+      {hasError && <Error />}
+      {!hasError && coocktailItems?.length === 0 && <Loading />}
+      {!hasError && coocktailItems?.length > 0 && coocktailItems.map(({ idDrink, strCategory, strAlcoholic, strDrinkThumb, strDrink }) => {
         return (
           <CocktailList key={`${idDrink}`} id={idDrink} title={strDrink} url={strDrinkThumb} alcoholic={strAlcoholic} category={strCategory} />
         )
-      }) : <Loading />}
+      })}
 
     </div>
   )
